8395d61565991b9891a43d3f2a7c155fd7d6a105
BiroJulia <biro.julianna@student.ms.sapientia.ro>
2023-08-25 16:15:05 +0300

    homescreen edit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:sornaplo/screens/popUpEdit.dart';
@@ -35,10 +39,14 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void addBrew(Map<String, dynamic> brew) {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
     setState(() {
       savedBrewCards.add(brew);
     });
+    log("userId: $userId"); //debug purposes only!
     _firestore.collection('brews').add({
+      'userId': userId,
       'name': brew['name'],
       'date': brew['date'],
       'type': brew['type'],
@@ -54,7 +62,17 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> _fetchBrews() async {
-    _brewStream = _firestore.collection('brews').snapshots();
+    // _brewStream = _firestore.collection('brews').snapshots();
+    // // return _brewStream;
+    setState(() {});
+
+    // Get the current user's ID
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    _brewStream = _firestore
+        .collection('brews')
+        .where('userId', isEqualTo: userId)
+        .snapshots();
   }
 
   @override
@@ -87,144 +105,155 @@ class _HomeScreenState extends State<HomeScreen> {
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
-          }
-
-          List<Widget> brewCards = [];
-
-          for (var doc in snapshot.data!.docs) {
-            var brewData = doc.data() as Map<String, dynamic>;
-            brewCards.add(
-              Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: ListTile(
-                  contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        brewData["name"],
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.white30,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              _getMonth(brewData["date"].toDate().month),
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                                color: const Color.fromARGB(255, 101, 101, 101),
-                              ),
-                            ),
-                            Text(
-                              "${brewData["date"].toDate().day}",
-                              style: TextStyle(
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold,
-                                color: const Color.fromARGB(255, 101, 101, 101),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Column(
+          } else {
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                      title: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            brewData["type"],
-                            textAlign: TextAlign.end,
+                            snapshot.data!.docs[index]["name"],
                             style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
-                          RatingBar.builder(
-                            initialRating: brewData["rating"].toDouble(),
-                            minRating: 1,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemSize: 24,
-                            itemPadding: EdgeInsets.symmetric(vertical: 2.0),
-                            itemBuilder: (context, _) => Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                            ),
-                            onRatingUpdate: (rating) {
-                              _firestore
-                                  .collection('brews')
-                                  .doc(doc.id)
-                                  .update({'rating': rating});
-                            },
-                          ),
-                          SizedBox(width: 8),
-                          brewData["rating"] > 2.5
-                              ? Icon(Icons.thumb_up, color: Colors.green)
-                              : Icon(Icons.thumb_down, color: Colors.red),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }
-
-          return savedBrewCards.isEmpty
-              ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/images/smallbeericon.png",
-                        width: 100,
-                        height: 100,
-                        color: Colors.black26,
-                      ),
-                      SizedBox(height: 10),
-                      Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-                        child: Text(
-                          "Üdvözlet a Sörnaplóban! \n\n Hogy ez az oldal ne legyen ilyen üres, főzz egy új sört és vezess itt naplót.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black38,
-                            fontWeight: FontWeight.w400,
+                      subtitle: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.white30,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  _getMonth(snapshot.data!.docs[index]["date"]
+                                      .toDate()
+                                      .month),
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color.fromARGB(
+                                        255, 101, 101, 101),
+                                  ),
+                                ),
+                                Text(
+                                  "${snapshot.data!.docs[index]["date"].toDate().day}",
+                                  style: TextStyle(
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                    color: const Color.fromARGB(
+                                        255, 101, 101, 101),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
+                          SizedBox(width: 8),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                snapshot.data!.docs[index]["type"],
+                                textAlign: TextAlign.end,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              RatingBar.builder(
+                                initialRating: snapshot
+                                    .data!.docs[index]["rating"]
+                                    .toDouble(),
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemSize: 24,
+                                itemPadding:
+                                    EdgeInsets.symmetric(vertical: 2.0),
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  _firestore
+                                      .collection('brews')
+                                      .doc(snapshot.data!.docs[index].id)
+                                      .update({'rating': rating});
+                                },
+                              ),
+                              SizedBox(width: 8),
+                              snapshot.data!.docs[index]["rating"] > 2.5
+                                  ? Icon(Icons.thumb_up, color: Colors.green)
+                                  : Icon(Icons.thumb_down, color: Colors.red),
+                            ],
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )
-              : ListView(
-                  children: brewCards,
-                );
+                    ),
+                  );
+                });
+          }
+          List<Widget> brewCards = [];
+
+          // for (var doc in snapshot.data!.docs) {
+          //   var brewData = doc.data() as Map<String, dynamic>;
+          //   brewCards.add(
+
+          //   );
+          // }
+
+          // return savedBrewCards.isEmpty
+          //     ? Center(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           children: [
+          //             Image.asset(
+          //               "assets/images/smallbeericon.png",
+          //               width: 100,
+          //               height: 100,
+          //               color: Colors.black26,
+          //             ),
+          //             SizedBox(height: 10),
+          //             Container(
+          //               padding:
+          //                   EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+          //               child: Text(
+          //                 "Üdvözlet a Sörnaplóban! \n\n Hogy ez az oldal ne legyen ilyen üres, főzz egy új sört és vezess itt naplót.",
+          //                 textAlign: TextAlign.center,
+          //                 style: TextStyle(
+          //                   fontSize: 18,
+          //                   color: Colors.black38,
+          //                   fontWeight: FontWeight.w400,
+          //                 ),
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       )
+          //     : ListView(
+          //         children: brewCards,
+          //       );
         },
       ),
     );
   }
 }
-
 
 
       //     Center(
