e4373f8f5971117644d6e3bc9842a991be29c4f8
BiroJulia <biro.julianna@student.ms.sapientia.ro>
2024-06-19 21:36:18 +0300

    event details design

--- lib/screens/event_details_screen.dart
+++ lib/screens/event_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../utils/colors_utils.dart';
 
 class EventDetailsScreen extends StatelessWidget {
   final String eventId;
@@ -14,10 +15,12 @@ class EventDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Esemény Részletek'),
+        backgroundColor: hexStringToColor("EC9D00"),
+        elevation: 2,
       ),
       body: Column(
         children: [
-          FutureBuilder(
+          FutureBuilder<DocumentSnapshot>(
             future: FirebaseFirestore.instance.collection('events').doc(eventId).get(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
@@ -26,25 +29,73 @@ class EventDetailsScreen extends StatelessWidget {
               var event = snapshot.data;
               return Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(event!['name'], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 8),
-                    Text('Időpont: ${event['time']}'),
-                    SizedBox(height: 8),
-                    Text('Helyszín: ${event['location']}'),
-                    SizedBox(height: 8),
-                    Text('Leírás: ${event['description']}'),
-                    SizedBox(height: 16),
-                    Text('Kommentek:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  ],
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10.0,
+                        offset: Offset(0.0, 10.0),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        event!['name'],
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Időpont: ${event['time']}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Helyszín: ${event['location']}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Leírás: ${event['description']}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               );
             },
           ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Kommentek:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
           Expanded(
-            child: StreamBuilder(
+            child: StreamBuilder<QuerySnapshot>(
               stream: FirebaseFirestore.instance.collection('events').doc(eventId).collection('comments').snapshots(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
@@ -55,9 +106,43 @@ class EventDetailsScreen extends StatelessWidget {
                   itemCount: comments.length,
                   itemBuilder: (context, index) {
                     var comment = comments[index];
-                    return ListTile(
-                      title: Text(comment['text']),
-                      subtitle: Text(comment['user']),
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 10.0,
+                              offset: Offset(0.0, 10.0),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              comment['user'],
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              comment['text'],
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     );
                   },
                 );
@@ -65,13 +150,30 @@ class EventDetailsScreen extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _commentController,
-                    decoration: InputDecoration(labelText: 'Komment'),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10.0,
+                          offset: Offset(0.0, 10.0),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _commentController,
+                      decoration: InputDecoration(
+                        hintText: 'Komment',
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
                 ),
                 IconButton(
@@ -83,6 +185,7 @@ class EventDetailsScreen extends StatelessWidget {
                     });
                     _commentController.clear();
                   },
+                  color: Colors.amber.shade200,
                 ),
               ],
             ),
--- lib/utils/eventPopUpEdit.dart
+++ lib/utils/eventPopUpEdit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
 class EventPopUpEdit extends StatefulWidget {
   @override
@@ -13,58 +15,180 @@ class _EventPopUpEditState extends State<EventPopUpEdit> {
   final _eventLocationController = TextEditingController();
   final _eventDescriptionController = TextEditingController();
 
+  double _calculateContainerHeight(String text) {
+    final numberOfLines = (text.split('\n').length).toDouble();
+    final lineHeight = 25.0;
+    return numberOfLines * (0.8 * lineHeight) + 30.0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Új Esemény Létrehozása'),
-      content: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextField(
-              controller: _eventNameController,
-              decoration: InputDecoration(labelText: 'Esemény Neve'),
-            ),
-            TextField(
-              controller: _eventTimeController,
-              decoration: InputDecoration(labelText: 'Időpont'),
-            ),
-            TextField(
-              controller: _eventLocationController,
-              decoration: InputDecoration(labelText: 'Helyszín'),
-            ),
-            TextField(
-              controller: _eventDescriptionController,
-              decoration: InputDecoration(labelText: 'Leírás'),
-            ),
-          ],
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Mégse'),
-        ),
-        TextButton(
-          onPressed: () async {
-            await FirebaseFirestore.instance.collection('events').add({
-              'name': _eventNameController.text,
-              'date': _selectedDay,
-              'time': _eventTimeController.text,
-              'location': _eventLocationController.text,
-              'description': _eventDescriptionController.text,
-            });
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 248, 229),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Új Esemény Létrehozása',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                '${DateFormat('yyyy.MM.dd.').format(_selectedDay)} - dátumra ',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              _buildAnimatedTextField("Esemény neve", _eventNameController.text, (value) {
+                setState(() {
+                  _eventNameController.text = value;
+                });
+              }),
+              SizedBox(height: 12.0),
+              _buildAnimatedTextField("Időpont", _eventTimeController.text, (value) {
+                setState(() {
+                  _eventTimeController.text = value;
+                });
+              }),
+              SizedBox(height: 12.0),
+              _buildAnimatedTextField("Helyszín", _eventLocationController.text, (value) {
+                setState(() {
+                  _eventLocationController.text = value;
+                });
+              }),
+              SizedBox(height: 12.0),
+              _buildAnimatedTextField("Leírás", _eventDescriptionController.text, (value) {
+                setState(() {
+                  _eventDescriptionController.text = value;
+                });
+              }),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber.shade200,
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      "Mégsem",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      var userId = FirebaseAuth.instance.currentUser?.uid;
+                      await FirebaseFirestore.instance.collection('events').add({
+                        'name': _eventNameController.text,
+                        'date': _selectedDay,
+                        'time': _eventTimeController.text,
+                        'location': _eventLocationController.text,
+                        'description': _eventDescriptionController.text,
+                        'userId': userId,
+                      });
 
-            _eventNameController.clear();
-            _eventTimeController.clear();
-            _eventLocationController.clear();
-            _eventDescriptionController.clear();
-            Navigator.of(context).pop();
-          },
-          child: Text('Mentés'),
+                      _eventNameController.clear();
+                      _eventTimeController.clear();
+                      _eventLocationController.clear();
+                      _eventDescriptionController.clear();
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber.shade200,
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      "Mentés",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        height: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ],
+      ),
+    );
+  }
+
+  Widget _buildAnimatedTextField(String hintText, String value, ValueChanged<String> onChanged) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        height: _calculateContainerHeight(value),
+        child: TextField(
+          onChanged: onChanged,
+          maxLines: null,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              fontSize: 18.0,
+              color: Colors.grey[400],
+            ),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+          ),
+        ),
+      ),
     );
   }
 }
